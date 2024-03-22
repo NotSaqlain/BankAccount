01f5e9c5ca33454df6c4035741354fe228927ea1
NotSaqlain <iamnotsaqlain@gmail.com>
2024-03-22 23:30:38 +0100

    update

--- array.cpp
+++ array.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
 class Array{
 private:
-    int v[];
+    int v[10];
 
 public:
 
@@ -13,15 +12,34 @@ public:
         cout << "404 not found" << endl;
     }
 
-    Array(int v[]) {
+    Array(int vet[]) {
+        for (int i = 0; i < 10; i++) {
+            v[i] = vet[i];
+        }
+    }
+    
+    void fill() {
         for (int i = 0; i < 10; i++) {
             v[i] = rand() % 100;
         }
     }
-
+    
     void stampa() {
         for (int i = 0; i < 10; i++) {
-            cout << v[i];
+            cout << v[i] << " - ";
+        }
+        cout << endl;
+    }
+    
+    void arrange() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (v[j] > v[i]) {
+                    int temp = v[j];
+                    v[j] = v[i];
+                    v[i] = temp;
+                }
+            }
         }
     }
 };
@@ -31,7 +49,11 @@ int main() {
     int v[10];
 
     Array vet(v);
-
+    
+    vet.fill();
+    vet.stampa();
+    
+    vet.arrange();
     vet.stampa();
 
 
